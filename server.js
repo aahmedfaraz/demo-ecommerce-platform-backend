a313c527a035ea69978b97da3d9886017eba8144
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2022-01-15 03:19:58 +0500

    Add Orders Route - GET - PUT - DELETE

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/products", require("./routes/products"));
 app.use("/api/carts", require("./routes/carts"));
+app.use("/api/orders", require("./routes/orders"));
 
 app.listen(PORT, () => {
   console.log(`Server has been started\nhttp://localhost:${PORT}`);
